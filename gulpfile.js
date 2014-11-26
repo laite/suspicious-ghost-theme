9431a79e85e5ca8be57e8995483be9be2bdf5719
Markus Klinga <laite@gmx.com>
2014-11-26 19:28:34 +0200

    add autoprefixer() to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
 	minify = require('gulp-minify-css'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload'),
+	autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
 	gulp.src('./src/scss/*.scss')
 		.pipe(sass())
+		.pipe(autoprefixer({cascade: false}))
 		.pipe(minify())
 		.pipe(gulp.dest('./assets/css'));
 });
